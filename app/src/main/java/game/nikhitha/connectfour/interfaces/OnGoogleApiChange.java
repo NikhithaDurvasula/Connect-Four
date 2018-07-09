502d1203bb31af73f5209514bd27e75f0a4b84c1
Nikhitha Durvasula <nikhithadurvasula69@gmail.com>
2018-07-08 22:00:30 -0700

    Update OnGoogleApiChange.java

--- app/src/main/java/game/nikhitha/connectfour/interfaces/OnGoogleApiChange.java
+++ app/src/main/java/game/nikhitha/connectfour/interfaces/OnGoogleApiChange.java
@@ -2,9 +2,6 @@ package game.nikhitha.connectfour.interfaces;
 
 import android.os.Bundle;
 
-/**
- * Created by karataev on 4/27/16.
- */
 public interface OnGoogleApiChange {
     void onConnectedApi(Bundle bundle);
 
